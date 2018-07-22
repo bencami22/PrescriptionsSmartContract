e30f8bc68a1b2aa07e44e9d607dc5644ddd312f8
Ben Camilleri <camilleriben@gmail.com>
2018-07-22 18:47:43 +0200

    Emit PrescriptionCreated when a prescription is added succesfully + formatting

--- Prescriptions.sol
+++ Prescriptions.sol
@@ -1,60 +1,67 @@
-pragma solidity ^0.4.0;
+pragma solidity ^ 0.4 .0;
 import "https://github.com/Arachnid/solidity-stringutils/strings.sol";
 
 contract Prescriptions {
-   using strings for *;
-    
-   struct Prescription {
+    using strings
+    for * ;
+
+    struct Prescription {
         address medicalPractitioner;
         address patient;
-        
+
         uint dateCreated;
-    
+
         string[] medications;
-    
+
         bool dispensed;
         address dispenser;
         uint dateDispensed;
-   }
-   
-   enum MedicalPractitionerType {PRESCRIBER, DISPENSER}
-   
-   struct MedicalPractitioner
-   {
-       address medicalPractitioner;
-       string licenseNumber;
-       MedicalPractitionerType medicalPractitionerType;
-   }
-    
-   Prescription[] internal prescriptions;
-   
-   MedicalPractitioner[] internal medicalPractitioners;
-   
-   function addMedicalPractitioner(string licenseNumber, MedicalPractitionerType medicalPractitionerType) public returns (uint)
-   {
-       medicalPractitioners.length++;
-       medicalPractitioners[medicalPractitioners.length-1].medicalPractitioner=msg.sender;
-       medicalPractitioners[medicalPractitioners.length-1].licenseNumber=licenseNumber;
-       medicalPractitioners[medicalPractitioners.length-1].medicalPractitionerType=medicalPractitionerType;
-       
-       return medicalPractitioners.length-1;
-   }
-    
-   function addPrescription(address patient, string medicationsCSV) public returns(uint) {
+    }
+
+    enum MedicalPractitionerType {
+        PRESCRIBER,
+        DISPENSER
+    }
+
+    struct MedicalPractitioner {
+        address medicalPractitioner;
+        string licenseNumber;
+        MedicalPractitionerType medicalPractitionerType;
+    }
+
+    Prescription[] internal prescriptions;
+
+    MedicalPractitioner[] internal medicalPractitioners;
+
+    function addMedicalPractitioner(string licenseNumber, MedicalPractitionerType medicalPractitionerType) public returns(uint) {
+        medicalPractitioners.length++;
+        medicalPractitioners[medicalPractitioners.length - 1].medicalPractitioner = msg.sender;
+        medicalPractitioners[medicalPractitioners.length - 1].licenseNumber = licenseNumber;
+        medicalPractitioners[medicalPractitioners.length - 1].medicalPractitionerType = medicalPractitionerType;
+
+        return medicalPractitioners.length - 1;
+    }
+
+    event PrescriptionCreated(address medicalPractitioner, address patient, uint dateCreated, string medications);
+
+    function addPrescription(address patient, string medicationsCSV) public returns(uint) {
 
         prescriptions.length++;
-        prescriptions[prescriptions.length-1].patient=patient;
-        
+        prescriptions[prescriptions.length - 1].patient = patient;
+
         var delim = ",".toSlice();
-        prescriptions[prescriptions.length-1].medications = new string[](medicationsCSV.toSlice().count(delim) + 1);
-        for(uint i = 0; i < prescriptions[prescriptions.length-1].medications.length; i++) {
-            prescriptions[prescriptions.length-1].medications[i] = medicationsCSV.toSlice().split(delim).toString();
+        prescriptions[prescriptions.length - 1].medications = new string[](medicationsCSV.toSlice().count(delim) + 1);
+        for (uint i = 0; i < prescriptions[prescriptions.length - 1].medications.length; i++) {
+            prescriptions[prescriptions.length - 1].medications[i] = medicationsCSV.toSlice().split(delim).toString();
         }
 
-        prescriptions[prescriptions.length-1].medicalPractitioner = msg.sender;
-        prescriptions[prescriptions.length-1].dateCreated = now;
-        
-        return prescriptions.length-1;
+        prescriptions[prescriptions.length - 1].medicalPractitioner = msg.sender;
+        prescriptions[prescriptions.length - 1].dateCreated = now;
+
+        //emit the event to any subscribers
+        emit PrescriptionCreated(prescriptions[prescriptions.length - 1].medicalPractitioner, prescriptions[prescriptions.length - 1].patient, prescriptions[prescriptions.length - 1].dateCreated, medicationsCSV);
+
+        return prescriptions.length - 1;
     }
 
     function getPrescriptionCount() public constant returns(uint) {
@@ -63,22 +70,21 @@ contract Prescriptions {
 
     function dispensePrescription(uint index, address patient) public returns(string) {
         //prescriptions.length>index
-        
+
         //prescriptions[index].dispensed
-        
-        if(patient==prescriptions[index].patient)
-        {
-            prescriptions[index].dispensed=true;
-            prescriptions[index].dispenser=msg.sender;
-            prescriptions[index].dateDispensed=now;
-            
+        var returnStr = "Prescription cannot be dispensed";
+        if (patient == prescriptions[index].patient) {
+            prescriptions[index].dispensed = true;
+            prescriptions[index].dispenser = msg.sender;
+            prescriptions[index].dateDispensed = now;
+
             //concatenation to build the csv, probably costly, to re visit.
-            var returnStr="";
-            for(uint i=0;i<prescriptions[index].medications.length;i++)
-            {
-                returnStr=prescriptions[index].medications[i].toSlice().concat(returnStr.toSlice());
+
+            for (uint i = 0; i < prescriptions[index].medications.length; i++) {
+                returnStr = prescriptions[index].medications[i].toSlice().concat(returnStr.toSlice());
             }
-            return returnStr;
         }
+        return returnStr;
+
     }
 }
