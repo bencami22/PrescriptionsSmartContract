9452fcb84c0a7f9e3d6c600ee692c33ffae2fa15
Ben Camilleri <camilleriben@gmail.com>
2018-07-23 22:38:45 +0200

    Implemented getPrescription()

--- Prescriptions.sol
+++ Prescriptions.sol
@@ -49,31 +49,46 @@ contract Prescriptions {
     function addPrescription(string reference, address patient, string medicationsCSV) public returns(uint) {
 
         uint index= prescriptionReferences[reference.toSlice().concat(toString(patient).toSlice())];
-        if(index == 0) //0 means doesn't exist (index 0 is also taken by default unusuable value in cconstructor)
-        {
-            prescriptions.length++;
-            prescriptions[prescriptions.length - 1].patient = patient;
-    
-            var delim = ",".toSlice();
-            prescriptions[prescriptions.length - 1].medications = new string[](medicationsCSV.toSlice().count(delim) + 1);
-            for (uint i = 0; i < prescriptions[prescriptions.length - 1].medications.length; i++) {
-                prescriptions[prescriptions.length - 1].medications[i] = medicationsCSV.toSlice().split(delim).toString();
-            }
-    
-            prescriptions[prescriptions.length - 1].medicalPractitioner = msg.sender;
-            prescriptions[prescriptions.length - 1].dateCreated = now;
-    
-            prescriptionReferences[reference.toSlice().concat(toString(patient).toSlice())] = prescriptions.length-1;
-    
-            //emit the event to any subscribers
-            emit PrescriptionCreated(reference, prescriptions[prescriptions.length - 1].medicalPractitioner, prescriptions[prescriptions.length - 1].patient, prescriptions[prescriptions.length - 1].dateCreated, medicationsCSV);
-            
-            return prescriptions.length - 1;
+        if(index != 0) throw; //0 means doesn't exist (index 0 is also taken by default unusuable value in cconstructor)
+        
+        prescriptions.length++;
+        prescriptions[prescriptions.length - 1].patient = patient;
+
+        var delim = ",".toSlice();
+        prescriptions[prescriptions.length - 1].medications = new string[](medicationsCSV.toSlice().count(delim) + 1);
+        for (uint i = 0; i < prescriptions[prescriptions.length - 1].medications.length; i++) {
+            prescriptions[prescriptions.length - 1].medications[i] = medicationsCSV.toSlice().split(delim).toString();
         }
+
+        prescriptions[prescriptions.length - 1].medicalPractitioner = msg.sender;
+        prescriptions[prescriptions.length - 1].dateCreated = now;
+
+        prescriptionReferences[reference.toSlice().concat(toString(patient).toSlice())] = prescriptions.length-1;
+
+        //emit the event to any subscribers
+        emit PrescriptionCreated(reference, prescriptions[prescriptions.length - 1].medicalPractitioner, prescriptions[prescriptions.length - 1].patient, prescriptions[prescriptions.length - 1].dateCreated, medicationsCSV);
+        
+        return prescriptions.length - 1;
+        
     }
 
     function getPrescriptionCount() public constant returns(uint) {
         return prescriptions.length;
+    }
+    
+    function getPrescription(string reference, address patient) public constant returns(address medicalPractitioner,
+     uint dateCreated, string medications, bool dispensed, address dispenser, uint dateDispensed) {
+        uint index = prescriptionReferences[reference.toSlice().concat(toString(patient).toSlice())];
+        if(index == 0) throw; //0 means doesn't exist (index 0 is also taken by default unusuable value in cconstructor)
+        
+        Prescription memory p=prescriptions[index];
+        string memory medicationsCSV = "";
+        //concatenation to build the csv, probably costly, to re visit.
+        for (uint i = 0; i < prescriptions[index].medications.length; i++) {
+            medicationsCSV = prescriptions[index].medications[i].toSlice().concat(medicationsCSV.toSlice());
+        }
+        
+        return (p.medicalPractitioner, p.dateCreated, medicationsCSV, p.dispensed, p.dispenser, p.dateDispensed);
     }
     
     event PrescriptionDispensed(string reference, address dispenser, address patient, uint dateDispensed, string medicationsCSV);
@@ -86,17 +101,17 @@ contract Prescriptions {
         if (patient != prescriptions[index].patient) throw; //check if patient prescribed is the patient being dispensed to.
         if(prescriptions[index].dispensed) throw; //check if not already dispensed.
         
-        string memory returnStr="";
+        string memory medicationsCSV="";
         prescriptions[index].dispensed = true;
         prescriptions[index].dispenser = msg.sender;
         prescriptions[index].dateDispensed = now;
 
         //concatenation to build the csv, probably costly, to re visit.
         for (uint i = 0; i < prescriptions[index].medications.length; i++) {
-            returnStr = prescriptions[index].medications[i].toSlice().concat(returnStr.toSlice());
+            medicationsCSV = prescriptions[index].medications[i].toSlice().concat(medicationsCSV.toSlice());
         }
         
-        emit PrescriptionDispensed(reference, prescriptions[index].dispenser,patient, prescriptions[index].dateDispensed, returnStr);
+        emit PrescriptionDispensed(reference, prescriptions[index].dispenser,patient, prescriptions[index].dateDispensed, medicationsCSV);
     }
     
     function toString(address x) returns (string) {
